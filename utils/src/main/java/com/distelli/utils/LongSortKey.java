5a4eaa06bed57940e84ea61d346081d92680585a
Brian Maher <brian@brimworks.com>
2017-02-15 11:15:10 -0800

    Rename LongToSortKey to just be LongSortKey, add sortKeyToLong() method.

---
+++ utils/src/main/java/com/distelli/utils/LongSortKey.java
@@ -0,0 +1,54 @@
+package com.distelli.utils;
+
+/**
+ * B64 encode a long. Useful for sort keys that are sorced from a long.
+ */
+public class LongSortKey {
+    private static final char[] ALPHABET =
+        "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        .toCharArray();
+
+    public static String longToSortKey(long num) {
+        // ceil(64bits / 6bits) => 11
+        char[] result = new char[11];
+
+        int pos = 0;
+        for ( int shift=60; shift >= 0; shift -= 6 ) {
+            result[pos++] = ALPHABET[(int)(num >>> shift) & 0x3F];
+        }
+        return new String(result);
+    }
+
+    public static Long sortKeyToLong(String sortKey) {
+        if ( null == sortKey ) return null;
+        if ( sortKey.length() != 11 ) {
+            throw new IllegalArgumentException(
+                "Invalid longSortKey, must be string of length 11, got length="+sortKey.length());
+        }
+        char[] chrs = sortKey.toCharArray();
+        long result = 0;
+        int shift = 60;
+        for ( int i=0; i < chrs.length; i++, shift -= 6 ) {
+            result |= ord(chrs[i]) << shift;
+        }
+        return result;
+    }
+
+    private static long ord(char ch) {
+        if ( ch >= '.' ) {
+            if ( ch <= '9' ) {
+                return ch - '.';
+            } else if ( ch <= 'Z' ) {
+                if ( ch >= 'A' ) {
+                    return 12 + (ch - 'A');
+                }
+            } else if ( ch <= 'z' ) {
+                if ( ch >= 'a' ) {
+                    return 12 + 26 + (ch - 'a');
+                }
+            }
+        }
+        throw new IllegalArgumentException(
+            "Invalid longSortKey, contained char='"+ch+"'");
+    }
+}
--- utils/src/test/java/com/distelli/utils/TestLongSortKey.java
+++ utils/src/test/java/com/distelli/utils/TestLongSortKey.java
@@ -4,18 +4,18 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
-import static com.distelli.utils.LongToSortKey.longToSortKey;
+import static com.distelli.utils.LongSortKey.*;
 
-public class TestLongToSortKey
+public class TestLongSortKey
 {
     @Test
     public void test() {
-        final int range = 10;
+        final int range = 10000;
         // Zero should always be sorted first:
         long num = 0;
-        String lastSortKey = longToSortKey(num++);
+        String lastSortKey = testLongToSortKey(num++);
         while ( num < range ) {
-            String sortKey = longToSortKey(num++);
+            String sortKey = testLongToSortKey(num++);
             assertThat(sortKey, greaterThan(lastSortKey));
             lastSortKey = sortKey;
         }
@@ -23,23 +23,29 @@ public class TestLongToSortKey
         num = -range + Long.MAX_VALUE;
         boolean done = false;
         while ( num > 0 ) { // wait for overflow.
-            String sortKey = longToSortKey(num++);
+            String sortKey = testLongToSortKey(num++);
             assertThat(sortKey, greaterThan(lastSortKey));
             lastSortKey = sortKey;
         }
         // Negatives are next and should be in 2s complement form:
         num = Long.MIN_VALUE;
         while ( num <= Long.MIN_VALUE + range ) {
-            String sortKey = longToSortKey(num++);
+            String sortKey = testLongToSortKey(num++);
             assertThat(sortKey, greaterThan(lastSortKey));
             lastSortKey = sortKey;
         }
         // Finally, the negatives counting before 0:
         num = -range;
         while ( num < 0 ) {
-            String sortKey = longToSortKey(num++);
+            String sortKey = testLongToSortKey(num++);
             assertThat(sortKey, greaterThan(lastSortKey));
             lastSortKey = sortKey;
         }
+    }
+
+    private String testLongToSortKey(long num) {
+        String sortKey = longToSortKey(num);
+        assertEquals(sortKey, num, sortKeyToLong(sortKey).longValue());
+        return sortKey;
     }
 }
