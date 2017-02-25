20c5c9ee05653f9320bccaa6da75784ad1e578d5
Rahul Singh <rsingh@distelli.com>
2017-02-25 11:32:00 -0800

    Added Composite Key

---
+++ utils/src/main/java/com/distelli/utils/CompositeKey.java
@@ -0,0 +1,113 @@
+/*
+  $Id: $
+  @file CompositeKey.java
+  @brief Contains the CompositeKey.java class
+
+  @author Rahul Singh [rsingh]
+  Copyright (c) 2013, Distelli Inc., All Rights Reserved.
+*/
+package com.distelli.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class CompositeKey
+{
+    public static final String DELIM = "\u001e";
+
+    public static class Builder
+    {
+        private List<String> parts = new ArrayList<String>();
+        public Builder withStrings(String... parts)
+        {
+            for(String part : parts)
+                withString(part);
+            return this;
+        }
+
+        public Builder withLongs(long... parts)
+        {
+            for(long part : parts)
+                withLong(part);
+            return this;
+        }
+
+        public Builder withString(String part)
+        {
+            this.parts.add(part);
+            return this;
+        }
+
+        public Builder withLong(long part)
+        {
+            this.parts.add(LongSortKey.longToSortKey(part));
+            return this;
+        }
+
+        public String build()
+        {
+            StringJoiner joiner = new StringJoiner(DELIM);
+            for(String part : this.parts)
+            {
+                if(part == null)
+                    part = "";
+                joiner.add(part);
+            }
+            return joiner.toString();
+        }
+
+        public String buildPrefix()
+        {
+            return String.format("%s\u001e", build());
+        }
+    }
+
+    public CompositeKey()
+    {
+
+    }
+
+    public static String build(String... parts)
+    {
+        return new Builder().withStrings(parts).build();
+    }
+
+    public static String build(long... parts)
+    {
+        return new Builder().withLongs(parts).build();
+    }
+
+    public static long[] splitLongs(String key)
+    {
+        String[] strParts = key.split(DELIM);
+        int index = 0;
+        long[] longParts = new long[strParts.length];
+        for(String part : strParts)
+        {
+            longParts[index] = LongSortKey.sortKeyToLong(part);
+            index++;
+        }
+        return longParts;
+    }
+
+    public static String buildPrefix(String... parts)
+    {
+        return new Builder().withStrings(parts).buildPrefix();
+    }
+
+    public static String buildPrefix(long... parts)
+    {
+        return new Builder().withLongs(parts).buildPrefix();
+    }
+
+    public static String[] split(String key)
+    {
+        return key.split(DELIM);
+    }
+
+    public static String[] split(String key, int limit)
+    {
+        return key.split(DELIM, limit);
+    }
+}
---
+++ utils/src/test/java/com/distelli/utils/TestCompositeKey.java
@@ -0,0 +1,97 @@
+/*
+  $Id: $
+  @file TestCompositKey.java
+  @brief Contains the TestCompositKey.java class
+
+  @author Rahul Singh [rsingh]
+  Copyright (c) 2013, Distelli Inc., All Rights Reserved.
+*/
+package com.distelli.utils;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class TestCompositeKey
+{
+    @Test
+    public void testBuildStrings()
+    {
+        String[] parts = { "a", "b", "c"};
+        String key = CompositeKey.build(parts);
+        assertThat(key, equalTo("a\u001eb\u001ec"));
+    }
+
+    @Test
+    public void testBuildLongs()
+    {
+        long[] parts = { 1l, 2l, 3l};
+        String key = CompositeKey.build(parts);
+        assertThat(key, equalTo("........../\u001e..........0\u001e..........1"));
+    }
+
+    @Test
+    public void testBuildMixed()
+    {
+        String partA = "a";
+        long part2 = 2l;
+        String partB = "b";
+        String key = new CompositeKey.Builder()
+        .withString(partA)
+        .withLong(part2)
+        .withString(partB)
+        .build();
+
+        assertThat(key, equalTo("a\u001e..........0\u001eb"));
+    }
+
+    @Test
+    public void testSplitStrings()
+    {
+        String key = "a\u001eb\u001ec";
+        String[] parts = CompositeKey.split(key);
+        assertThat(parts.length, equalTo(3));
+        assertThat(parts[0], equalTo("a"));
+        assertThat(parts[1], equalTo("b"));
+        assertThat(parts[2], equalTo("c"));
+    }
+
+    @Test
+    public void testSplitLongs()
+    {
+        String key = "........../\u001e..........0\u001e..........1";
+        long[] parts = CompositeKey.splitLongs(key);
+        assertThat(parts.length, equalTo(3));
+        assertThat(parts[0], equalTo(1l));
+        assertThat(parts[1], equalTo(2l));
+        assertThat(parts[2], equalTo(3l));
+    }
+
+    @Test
+    public void testSplitStringsLimit()
+    {
+        String key = "a\u001eb\u001eHello\u001eWorld";
+        String[] parts = CompositeKey.split(key, 3);
+        assertThat(parts.length, equalTo(3));
+        assertThat(parts[0], equalTo("a"));
+        assertThat(parts[1], equalTo("b"));
+        assertThat(parts[2], equalTo("Hello\u001eWorld"));
+    }
+
+    @Test
+    public void buildPrefix()
+    {
+        String[] parts = { "a", "b", "c"};
+        String key = CompositeKey.buildPrefix(parts);
+        assertThat(key, equalTo("a\u001eb\u001ec\u001e"));
+    }
+
+    @Test
+    public void testBuildPrefixLongs()
+    {
+        long[] parts = { 1l, 2l, 3l};
+        String key = CompositeKey.buildPrefix(parts);
+        assertThat(key, equalTo("........../\u001e..........0\u001e..........1\u001e"));
+    }
+}
