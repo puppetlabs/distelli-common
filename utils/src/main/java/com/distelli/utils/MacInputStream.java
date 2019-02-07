c0eb1972c14e7b22ac62ea462cb2e6b6b49b3600
Brian Maher <brian.maher@puppet.com>
2019-02-07 09:37:29 -0800

    Add MacInputStream utility.

---
+++ utils/src/main/java/com/distelli/utils/MacInputStream.java
@@ -0,0 +1,37 @@
+package com.distelli.utils;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+import javax.crypto.Mac;
+import java.io.IOException;
+
+public class MacInputStream extends FilterInputStream {
+    private Mac _mac;
+    public MacInputStream(InputStream is, Mac mac) {
+        super(is);
+        _mac = mac;
+    }
+
+    public MacInputStream mac(Mac mac) {
+        _mac = mac;
+        return this;
+    }
+
+    @Override
+    public int read() throws IOException {
+        int ch = in.read();
+        if ( null != _mac && ch >= 0 ) {
+            _mac.update((byte)ch);
+        }
+        return ch;
+    }
+
+    @Override
+    public int read(byte[] buff, int off, int len) throws IOException {
+        int result = in.read(buff, off, len);
+        if ( null != _mac && result > 0 ) {
+            _mac.update(buff, off, result);
+        }
+        return result;
+    }
+}
